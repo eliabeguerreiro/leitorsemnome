1ac52adda048586580157d63d577f7e9237e68b6
Eliabe  Paz <eliabepaz.work@gmail.com>
2020-04-08 10:52:40 -0300

    commit on master

--- src/notificador.php
+++ src/notificador.php
@@ -4,7 +4,8 @@ function notification($tipo, $conexao) {
     $notificacao = [];
     foreach ($tipo as $valuer){
         $verifica_notificacao = "SELECT texto, imagem FROM notificacoes WHERE tipo='$valuer'";
-        $resultado[] = mysqli_query($conexao, $verifica_notificacao);
+        $result = mysqli_query($conexao, $verifica_notificacao);
+        $resultado[] = mysqli_fetch_assoc($result);
         $notificacao[] = $resultado;
     }
     return $notificacao;
--- src/painel.php
+++ src/painel.php
@@ -6,8 +6,8 @@ if(!empty($_SESSION['id'])){
 	echo "Olá ".$_SESSION['nome'].", Bem vindo <br>";
 	echo "<a href='sair.php'>Sair</a>";
 }
-$tipo = array(001);
-/* os numeros acima serão referencia aos ID's dos mangas e os as notificações de sistema e de mensagens terão dois zeros e o digito indentificador*/
+$tipo = array(1,2);
+/* os numeros acima serão referencia aos ID's dos mangas |notificações de sistema e mensagens serão respectivamente os difitos 1 e 2*/
 $notifica = notification($tipo, $conn);
 //o valor retornado é uma matris x por 2 sendo a notificação um texto e uma imagem
 foreach($notifica as $n){
